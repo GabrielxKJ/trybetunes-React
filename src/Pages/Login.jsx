bf8e30bc49cac5cc3b95c31a6d7366d95e9ae3e2
User <gabrielkleber006@gmail.com>
2021-09-19 22:20:18 -0300

    requisito 2.

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,10 +1,66 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import Loading from '../Components/Loading';
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      loading: false,
+      logged: false,
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  // função que salva o nome do usuário e a lógica do loading
+  async handleClick() {
+    const { name } = this.state;
+    this.setState({ loading: true });
+    await createUser({ name });
+    this.setState({ loading: false, logged: true });
+  }
+
+  // sempre que definir um elemento de form, setar um função handle com os valores do estado para interagir com eles.
+  handleChange({ target }) {
+    this.setState({
+      name: target.value,
+    });
+  }
+
   render() {
+    const { loading, name, logged } = this.state;
+    if (loading) {
+      return <Loading />;
+    }
+    if (logged) {
+      // Após a conclusão do carregamento a propriedade logged fica true e redireciona para a página Search.
+      return <Redirect to="/search" />;
+    }
+    const minChar = 3;
     return (
       <div data-testid="page-login">
-        <h4>Login</h4>
+        <form action="submit">
+          <input
+            type="text"
+            data-testid="login-name-input"
+            value={ name }
+            onChange={ this.handleChange }
+          />
+
+          <button
+            type="button"
+            onClick={ this.handleClick }
+            disabled={ name.length < minChar }
+            data-testid="login-submit-button"
+          >
+            Entrar
+          </button>
+        </form>
       </div>
     );
   }
