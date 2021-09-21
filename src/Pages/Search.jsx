fcd12703ae38479724d6b7352e6946c063e701af
User <gabrielkleber006@gmail.com>
2021-09-21 12:33:46 -0300

    requisito 5.

--- src/Pages/Search.jsx
+++ src/Pages/Search.jsx
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
 import Header from '../Components/Header';
+// import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      searchArtist: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    this.setState({
+      searchArtist: target.value,
+    });
+  }
+
   render() {
+    const { searchArtist } = this.state;
+    const minChar = 2;
     return (
       <>
         <Header />
         <div data-testid="page-search">
-          <h4>Search</h4>
+          <h1>Search</h1>
+          <input
+            data-testid="search-artist-input"
+            value={ searchArtist }
+            onChange={ this.handleChange }
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ searchArtist.length < minChar }
+          >
+            Pesquisar
+          </button>
         </div>
       </>
     );
